a58b3c78fdab5c401c195ebae8fc04a3e53157a4
chenzh <chenzh@1jbest.cn>
2017-06-21 14:08:29 +0800

    Initial commit

---
+++ config-resource/src/main/java/com/czh/ConfigResourceApplication.java
@@ -0,0 +1,12 @@
+package com.czh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigResourceApplication.class, args);
+	}
+}
